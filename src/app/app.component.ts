d990a85527764b6c188d4a751e70b7147ef7e990
Anurag Singh <anuragsingh.cs.it@gmail.com>
2020-05-17 14:47:31 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'india-covid19';
+export class AppComponent implements OnInit{
+  title = 'India-Covid19';
+  constructor(private http: HttpClient) { }
+   posts:any;
+  ngOnInit(): void {
+this.http.get('https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true').subscribe(posts => {
+
+    this.posts = posts;
+    console.log(this.posts);
+    });
+   
+  }
+
 }
